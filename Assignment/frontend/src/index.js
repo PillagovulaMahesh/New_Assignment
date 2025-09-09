e39ce8f7ad5b0df88e6c3ae91e8e42e930d5414b
PILLAGOVLA MAHESH <137051507+PillagovulaMahesh@users.noreply.github.com>
2025-09-09 19:38:34 +0530

    Update index.js

---
+++ Assignment/frontend/src/index.js
@@ -0,0 +1,16 @@
+// frontend/src/index.js
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { AuthProvider } from "./context/AuthContext";
+import "./index.css"; // Tailwind or global styles
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
+);
