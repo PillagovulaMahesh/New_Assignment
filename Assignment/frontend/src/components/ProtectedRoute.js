50310fe4f157990c7208a358214c8cd3b7aa0862
PILLAGOVLA MAHESH <137051507+PillagovulaMahesh@users.noreply.github.com>
2025-09-09 19:33:24 +0530

    Update ProtectedRoute.js

---
+++ Assignment/frontend/src/components/ProtectedRoute.js
@@ -0,0 +1,20 @@
+// frontend/src/components/ProtectedRoute.js
+import React from "react";
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children, allowedRoles }) => {
+  const token = localStorage.getItem("token");
+  const user = JSON.parse(localStorage.getItem("user")); // saved after login
+
+  if (!token || !user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (allowedRoles && !allowedRoles.includes(user.role)) {
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
